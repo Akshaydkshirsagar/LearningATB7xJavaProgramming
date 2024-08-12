321ef25f2576f1b272611714949fd464e42a2981
Akshaydkshirsagar <4382ak@gmail.com>
2024-08-13 00:42:04 +0530

    All files are added

---
+++ src/Ex_070724/Lab004.java
@@ -0,0 +1,92 @@
+package Ex_070724;
+
+public class Lab004 {
+    // Data types
+
+    public static void main (String[] args){
+
+
+        // Byte- 1byte   -128 to 127
+
+        byte b= -122;
+        byte b1= 111;
+        System.out.println("Value of b is: "+b);
+        System.out.println("Value of b1 is: "+b1);
+
+
+        // short - 2bytes  -32768 to 32767
+
+        short s= -22122;
+        short s1= 30911;
+        System.out.println("Value of s is: "+s);
+        System.out.println("Value of s1 is: "+s1);
+
+
+        //Int 4 bytes
+        int i=4223234;
+        int i1= -432243244;
+
+        System.out.println("Value of i is: "+i);
+        System.out.println("Value of i1 is: "+i1);
+
+        // Long 8 bytes
+
+        long l= 442464645646l;
+        long l1=-9086966977l;
+
+        System.out.println("Value of l is: "+l);
+        System.out.println("Value of l1 is: "+l1);
+
+        // Float 4 bytes
+
+        float f= 4567.98765432f;
+        float f1= -453535.529456789f;
+
+        System.out.println("Value of f is: "+f);
+        System.out.println("Value of f1 is: "+f1);
+
+        // Double 8 bytes
+
+        double d= 2805725729520.352525252;
+        double d1= -48574550842492404.44;
+
+        System.out.println("Value of d is: "+d);
+        System.out.println("Value of d1 is: "+d1);
+
+        // CHAR 2 bytes   65535
+
+        char c='A';
+        char c1='g';
+        char c2='1';
+        char c3='@';
+        char c4=')';
+        char c5='?';
+
+        System.out.println("Value of c is: "+c);
+        System.out.println("Value of c1 is: "+c1);
+        System.out.println("Value of c2 is: "+c2);
+        System.out.println("Value of c3 is: "+c3);
+        System.out.println("Value of c4 is: "+c4);
+        System.out.println("Value of c5 is: "+c5);
+
+        //String
+
+        String str="Akshay is pro trader";
+        String str1= "Jay HIND Jay TRADERS...!";
+        String str2="AKS";
+        String str3="hay";
+
+
+        System.out.println("String str is : "+str);
+        System.out.println("String str1 is : "+str1);
+        System.out.println("String str2+ str3 is : "+str2+str3);
+
+
+
+
+
+
+
+
+    }
+}
